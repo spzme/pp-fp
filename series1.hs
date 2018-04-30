f57cdc6df6c8eacaa3e0914ac9794ce7c22f328b
Frank <fgletharg@gmail.com>
2018-04-30 13:15:29 +0200

    Added series 1 and 2

---
+++ series1.hs
@@ -0,0 +1,134 @@
+import Test.QuickCheck
+import Data.Char
+
+f:: Integer -> Integer
+f x = 2*x^2 + 3 * x - 5
+
+total1 :: Int -> Int
+total1 0 = 0 
+total1 n = total1 (n-1) + n
+
+total2 :: Int -> Int
+total2 n = (n * ( n )) `div` 2
+
+prop_total n = (n >= 0) ==> total1 n == total2 n
+
+prop_plus :: Int -> Int -> Bool
+prop_plus x y = x + y == y + x
+
+prop_min :: Int -> Int -> Bool
+prop_min x y = x - y == y - x
+
+code :: Char -> Char
+code c | c `myelem` ['a' .. 'z'] = chr ((ord c + 10) `mod` 26 + 97)
+       | ord c >= 65 && ord c <= 90 = chr ((ord c + 16) `mod` 26 + 65)
+       | otherwise = c
+
+test1 = map code "hello"
+test2 = map code "Tomorrow evening, 8 o'clock in Amsterdam"
+
+
+interest a r 0 = a
+interest a r n = interest (a * (1+r/100)) r (n-1)
+
+
+discr :: Float -> Float -> Float -> Float
+discr a b c = b * b - 4 * a * c
+
+root1 :: Float -> Float -> Float -> Float
+root1 a b c
+    | discr a b c < 0 = error("Negative discriminant")
+    | otherwise = ((-1 * b + sqrt(discr a b c)) / (2 * a))
+
+root2 :: Float -> Float -> Float -> Float
+root2 a b c
+    | discr a b c < 0 = error("Negative discriminant")
+    | otherwise = ((-1 * b - sqrt(discr a b c)) / (2 * a))
+
+extrX :: Float -> Float -> Float -> Float
+extrX a b c = -b / (2*a)
+
+extrY :: Float -> Float -> Float -> Float
+extrY a b c = a * (extrX a b c ** 2) + b * extrX a b c + c
+
+mylength:: [a] -> Int
+mylength [] = 0
+mylength (x:xs) = mylength xs + 1 
+
+mysum:: (Num a) => [a] -> a
+mysum [] = 0
+mysum (x:xs) = mysum xs + x
+
+myreverse:: [a] -> [a]
+myreverse [] = []
+myreverse (x:xs) = myreverse xs ++ [x]
+
+mytake:: [a] -> Int -> [a]
+mytake [] n = []
+mytake _ 0  = []
+mytake (x:xs) n = [x] ++ mytake xs (n-1) 
+
+myelem:: (Eq a) => [a] -> a -> Bool
+myelem [] a = False
+myelem (x:xs) a | x == a    = True
+                | otherwise = myelem xs a
+
+myconcat:: [a] -> [a] -> [a]
+myconcat xs [] = xs
+myconcat xs (y:ys) = myconcat (xs ++ [y]) ys
+ 
+mymaximum:: (Num a, Ord a) => [a] -> a
+mymaximum [] = -1
+mymaximum [x] = x
+mymaximum (x:xs) | x > mymaximum xs = x
+                 | otherwise        = mymaximum xs
+
+myzip:: [a] -> [a] -> [(a,a)]
+myzip xs [] = []
+myzip [] ys = []
+myzip (x:xs) (y:ys) = [(x,y)] ++ myzip xs ys
+
+r:: (Num a) => a -> a -> [a]
+r a d = [a] ++ r (a+d) d
+
+r1:: (Num a) => a -> a -> Int -> a
+r1 a d 0 = a
+r1 a d n = r1 (a+d) d (n-1)
+
+totalr:: (Num a) => a -> a -> Int -> Int -> a
+totalr a d i j = mysum (map (r1 a d) [i..j])
+
+allEqual:: (Eq a) => [a] -> Bool
+allEqual [] = True
+allEqual [x] = True
+allEqual (x:x':xs) | x==x'     = allEqual (x':xs)
+                   | otherwise = False
+
+isAS:: (Num a, Eq a) => [a] -> Bool
+isAS xs = allEqual (differences xs)
+
+differences:: (Num a, Eq a) => [a] -> [a]
+differences [] = []
+differences [x] = []
+differences (x:x':xs) = [x-x'] ++ differences (x':xs)
+
+rowLengthEqual:: [[a]] -> Bool
+rowLengthEqual xs = allEqual (map mylength xs)
+
+rowTotals :: (Num a) => [[a]] -> [a]
+rowTotals xs = map mysum xs
+
+mytranspose :: [[a]] -> [[a]]
+mytranspose [] = []
+mytranspose [x] = transposeRow x
+mytranspose (x:xs) = zipWith (++) (transposeRow x) (mytranspose xs)
+
+mytranspose' :: [[a]] -> [[a]]
+mytranspose' xs = map $ head xs ++ transpose $ map $ tail xs 
+
+transposeRow :: [a] -> [[a]]
+transposeRow [] = []
+transposeRow (x:xs) = [[x]] ++ transposeRow xs
+
+colTotals :: (Num a) => [[a]] -> [a]
+colTotals xs = rowTotals (mytranspose xs)
---
+++ series2.hs
@@ -0,0 +1,166 @@
+import Data.Char
+import Data.List
+import Test.QuickCheck
+
+myfilter:: (a -> Bool) -> [a] -> [a]
+myfilter f [] = []
+myfilter f (x:xs) | f x = [x] ++ myfilter f xs
+                  | otherwise = myfilter f xs
+
+myfilter':: (a -> Bool) -> [a] -> [a]
+myfilter' f xs = [x | x <- xs, f x]
+
+myfoldl:: (a -> b -> a) -> a -> [b] -> a
+myfoldl f z [] = z
+myfoldl f z (x:xs) = myfoldl f z xs `f` x
+
+myfoldr:: (a -> b -> b) -> b -> [a] -> b
+myfoldr f z [] = z
+myfoldr f z (x:xs) = x `f` myfoldr f z xs
+
+myZipWith:: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith f _ [] = []
+myZipWith f [] _ = []
+myZipWith f (x:xs) (y:ys) = [f x y] ++ myZipWith f xs ys
+
+database = [("Barry Pooter", 21, "Male", "Zweinstein"),
+            ("Hermelientje", 19, "Female", "Hagrid's Hut"),
+            ("Herta", 33, "Female", "Utopia")]
+
+name (x,_,_,_) = x
+age (_,x,_,_) = x
+sex (_,_,x,_) = x
+por (_,_,_,x) = x     
+
+increaseAge n [] = []
+increaseAge n ((a,b,c,d):xs) = (a,b+n,c,d) : (increaseAge n xs)
+
+increaseAge' n xs = [(a,b+n,c,d) | (a,b,c,d) <- xs]
+
+hoInc n (a,b,c,d) = (a,b+n,c,d)
+increaseAge'' n xs = map (hoInc n) xs
+
+milfs [] = []
+milfs ((a,b,c,d):xs) | b > 30 && b<40 && c=="Female" = a : milfs xs
+                     | otherwise = milfs xs
+
+milfs' xs = [a | (a,b,c,d) <- xs, b > 30, b < 40, c == "Female"]
+
+milffilter (a,b,c,d) = b > 30 && b<40 && c=="Female" 
+
+milfs'' xs = map name (myfilter milffilter xs)
+
+howOld n [] = -1 --not in database
+howOld n ((a,b,c,d):xs) | map toLower n == map toLower a = b
+                        | otherwise = howOld n xs
+
+swapAgeAndName (a,b,c,d) = (b,a,c,d)
+
+sortedByAge xs = map swapAgeAndName $ sort $ map swapAgeAndName xs
+
+sieve (x:xs) = x : [n | n <- sieve xs, n `mod` x /= 0]
+
+isPrime n = null [x | x <- [2..n - 1], n `mod` x == 0]
+
+firstPrimes n = take n [x | x <- [3..], isPrime x]
+
+primesSmallerThan n = [x | x <- [3..n], isPrime x]
+
+dividers n = [x | x <- [2..n - 1], n `mod` x == 0]
+
+isPrime' n = length (dividers n) == 2
+
+pyth n = [(a,b,c) | a <-[2..n], b<-[2..n], c<-[2..n], a**2 + b**2 == c**2]
+
+--How to use GCD here?
+--pyth' n = [(a,b,c) | a <-[2..n], b<-[2..a], c<-[2..n], a**2 + b**2 == c**2, gcd a b == 1]
+
+increasing:: [Int] -> Bool
+increasing [] = True
+increasing [x] = True
+increasing (x:x':xs) | x < x' = increasing (x':xs)
+                     | otherwise = False
+
+--Cannot get types working
+-- average:: [Double] -> Double
+-- average xs = ((sum xs) / ((length xs)::Double))
+
+-- weaklyIncreasing:: [Double] -> Bool
+-- weaklyIncreasing [] = True
+-- weaklyIncreasing [x] = True
+-- weaklyIncreasing xs = (average (init xs) < last xs) && (weaklyIncreasing $ init xs)
+
+sublist:: (Eq a) => [a] -> [a] -> Bool
+sublist xs [] = False
+sublist xs (y:ys) | xs == take (length xs) (y:ys) = True
+                  | otherwise = sublist xs ys 
+
+partialsublist:: (Eq a) => [a] -> [a] -> Bool
+partialsublist [] _ = True
+partialsublist xs [] = False 
+partialsublist (x:xs) (y:ys) | x == y = partialsublist xs ys
+                             | otherwise = partialsublist (x:xs) ys
+
+bubble:: (Ord a) => [a] -> [a]
+bubble [] = []
+bubble [x] = [x]
+bubble (x:x':xs) | x<=x'       = x  : bubble (x':xs)
+                 | otherwise   = x' : bubble (x:xs)
+
+bsort:: (Ord a) => [a] -> [a]
+bsort [] = []
+bsort xs = bsort (init ys) ++ [last ys]
+       where
+           ys = bubble xs
+
+prop_bsort:: [Int] -> Bool
+prop_bsort xs = bsort xs == sort xs
+
+mmsort:: (Ord a) => [a] -> [a]
+mmsort [] = []
+mmsort [x] = [x]
+mmsort xs = mn : mmsort (xs \\ [mn,mx]) ++ [mx]
+             where
+                mn = minimum xs
+                mx = maximum xs
+
+prop_mmsort:: [Int] -> Bool
+prop_mmsort xs = mmsort xs == sort xs
+
+ins:: (Ord a) => a -> [a] -> [a]
+ins x [] = [x]
+ins x (y:ys) | x <= y = x : y : ys
+             | otherwise = y : ins x ys
+
+isort:: (Ord a) => [a] -> [a]
+isort [] = []
+isort (x:xs) = ins x (isort xs)
+
+prop_isort:: [Int] -> Bool
+prop_isort xs = isort xs == sort xs
+
+merge:: (Ord a) => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys) | x <= y = x : merge xs (y:ys)
+                    | otherwise = y : merge (x:xs) ys
+
+msort:: (Ord a) => [a] -> [a]
+msort [] = []
+msort [x] = [x]
+msort xs = merge (msort (fst xs)) (msort (snd xs))
+        where 
+            fst xs = take (length xs `div`2) xs
+            snd xs = drop (length xs `div`2) xs
+
+prop_msort:: [Int] -> Bool
+prop_msort xs = msort xs == sort xs
+ 
+qsort::(Ord a) => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = qsort [y | y <- xs, y <= x] ++ [x] ++ qsort [y | y <- xs, y > x]
+
+prop_qsort:: [Int] -> Bool
+prop_qsort xs = qsort xs == sort xs
+
+
